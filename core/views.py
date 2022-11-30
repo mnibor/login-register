f4497c35a44a3e461f57602a7419c1b9ee2db67a
Marcelo Robin <mrobin@OpenSuse.fibertel.com.ar>
2022-11-30 10:31:48 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+@login_required
+def products(request):
+    return render(request, 'core/products.html')
+
+def register(request):
+    data = {
+        'form': CustomUserCreationForm()
+    }
+
+    if request.method == 'POST':
+        user_creation_form = CustomUserCreationForm(data=request.POST)
+
+        if user_creation_form.is_valid():
+            user_creation_form.save()
+
+            user = authenticate(username=user_creation_form.cleaned_data['username'], password=user_creation_form.cleaned_data['password1'])
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'registration/register.html', data)
