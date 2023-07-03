0fda8de21d267c0a40b3f3980cd0be933a472af7
mnibor <marcelorobin@gmail.com>
2023-07-03 11:59:46 -0300

    todos los cambios

--- core/forms.py
+++ core/forms.py
@@ -3,13 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+	email = forms.EmailField(required=True)
 
-    class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Este correo electrónico ya está registrado.")
-        return email
+	class Meta:
+		model = User
+		fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+	def clean_email(self):
+		email = self.cleaned_data['email']
+
+		if User.objects.filter(email=email).exists():
+			raise forms.ValidationError('Este correo electrónico ya está registrado')
+		return email
